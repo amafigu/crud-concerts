e64b102b8fcba26ddd6e574e32de8efe1244721d
amafigu <amadeusfigue@gmail.com>
2024-04-24 16:55:29 +0200

    application running

---
+++ src/main/java/com/tierrayaire/TierrayaireApplication.java
@@ -0,0 +1,13 @@
+package com.tierrayaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierrayaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierrayaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tierrayaire/TierrayaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tierrayaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierrayaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
