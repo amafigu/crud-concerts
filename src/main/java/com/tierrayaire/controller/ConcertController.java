9f2d067264ba3b945fb48fadaa5cdd7b25c35898
amafigu <amadeusfigue@gmail.com>
2024-04-25 13:36:02 +0200

    Concert controller

---
+++ src/main/java/com/tierrayaire/controller/ConcertController.java
@@ -0,0 +1,63 @@
+package com.tierrayaire.controller;
+
+import com.tierrayaire.exception.ResourceNotFoundException;
+import com.tierrayaire.model.Concert;
+import com.tierrayaire.repository.ConcertRepository;
+import com.tierrayaire.service.ConcertService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+
+public class ConcertController {
+
+    private final ConcertService concertService;
+
+    @Autowired
+    public ConcertController(ConcertService concertService) {
+        this.concertService = concertService;
+    }
+
+    @GetMapping("/concerts")
+    public List<Concert> getAllConcerts() {
+        return concertService.findAll();
+    }
+
+    @GetMapping("/concerts/{id}")
+    public ResponseEntity<Concert> getConcertById(@PathVariable Long id) {
+        Concert concert = concertService.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Concert not found with id: " + id));
+        return ResponseEntity.ok(concert);
+    }
+
+    @PostMapping("/concerts")
+    public Concert createConcert(@Valid @RequestBody Concert concert) {
+        return concertService.save(concert);
+    }
+
+    @PutMapping("/concerts/{id}")
+    public ResponseEntity<Concert> updateConcert(@Valid @PathVariable Long id, @RequestBody Concert concertDetails) {
+        Concert updatedConcert = concertService.update(id, concertDetails);
+        return ResponseEntity.ok(updatedConcert);
+    }
+
+    @DeleteMapping("/concerts/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteConcert(@PathVariable Long id) {
+        Concert concert = concertService.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Concert not found with id: " + id));
+        concertService.delete(concert);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+}
